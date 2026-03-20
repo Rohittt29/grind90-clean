6b37a7ca85e6ba8ec07abe93cb11ccb6c3679e2b
Rohittt29 <143200490+Rohittt29@users.noreply.github.com>
2026-03-20 21:13:02 +0530

    password hide

--- src/main/java/com/grind90/entity/User.java
+++ src/main/java/com/grind90/entity/User.java
@@ -19,7 +19,7 @@ public class User {
     @Column(unique = true, nullable = false)
     private String email;
 
-    @JsonProperty("password") // maps JSON -> field
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY) // maps JSON -> field
     @Column(nullable = false)
     private String password;
 
